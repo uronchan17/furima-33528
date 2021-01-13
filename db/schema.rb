f1f89788ce688e75561ba2888ab87083826f0a38
uronchan17 <76198482+uronchan17@users.noreply.github.com>
2021-01-13 15:44:34 +0900

    nicknameカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_103002) do
+ActiveRecord::Schema.define(version: 2021_01_13_064314) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_01_12_103002) do
     t.string "last_name_kana"
     t.string "first_name_kana"
     t.date "birthday"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
