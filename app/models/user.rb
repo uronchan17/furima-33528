71398d8d4b5c01fa8c460f841a797775f77705d1
uronchan17 <76198482+uronchan17@users.noreply.github.com>
2021-01-13 14:51:47 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, unique: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角(カタカナ)文字を使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  
+  validates :birthday, presence: true
 end
