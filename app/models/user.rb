61f38af188b5816182b5904bcadaa3f95f8fd3b9
uronchan17 <76198482+uronchan17@users.noreply.github.com>
2021-01-13 14:57:50 +0900

    バリデーション名修正、applicationコントローラーのsyntaxエラー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, unique: true
+  validates :email, uniqueness: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
@@ -23,6 +23,6 @@ class User < ApplicationRecord
 
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
-  
+
   validates :birthday, presence: true
 end
