cae0142933aba2c23952bb28b6ec881fa54703bd
uronchan17 <76198482+uronchan17@users.noreply.github.com>
2021-01-14 12:35:48 +0900

    アクティブハッシュのアソシエーションの設定

--- app/models/product_listing.rb
+++ app/models/product_listing.rb
@@ -1,3 +1,9 @@
 class ProductListing < ApplicationRecord
   belings_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_fee
+  belongs_to :area
+  belongs_to :days_to_ship
 end
