738ed9d264e27736e279c515a82428a67355e040
uronchan17 <76198482+uronchan17@users.noreply.github.com>
2021-01-14 15:35:19 +0900

    ログイン状態でないときにnewアクションにアクセスするとログインページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_session, only: :new
   def index
   end
   
@@ -13,5 +14,12 @@ class ItemsController < ApplicationController
   def items_params
     params.require(:product_listing).permit(:product_name, :explain, :category_id, :state_id, :delivery_id, :area_id, :date_id, :fee).merge(user_id: current.user.id)
   end
+
+  def move_to_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 
 end
