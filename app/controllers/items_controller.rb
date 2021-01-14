5a41e2564cbe632094033a3f9351ba6e4bb4abd2
uronchan17 <76198482+uronchan17@users.noreply.github.com>
2021-01-14 15:22:14 +0900

    createアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,14 @@ class ItemsController < ApplicationController
   
   def new
   end
+
+  def create
+    ProductListing.create(items_params)
+  end
+
+  private
+  def items_params
+    params.require(:product_listing).permit(:product_name, :explain, :category_id, :state_id, :delivery_id, :area_id, :date_id, :fee).merge(user_id: current.user.id)
+  end
+
 end
