c6fa3167cb764eff1d47d72a7bc613a9011e80ee
uronchan17 <76198482+uronchan17@users.noreply.github.com>
2021-01-15 15:38:39 +0900

    バリデーション記載の簡略化

--- app/models/product_listing.rb
+++ app/models/product_listing.rb
@@ -7,18 +7,17 @@ class ProductListing < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :area
   belongs_to :days_to_ship
-
-  validates :product_name, presence: true
-  validates :explain, presence: true
-  with_options numericality: { other_than: 1 }, presence:true do
-    validates :category_id
-    validates :state_id
-    validates :delivery_fee_id
-    validates :days_to_ship_id
+  with_options presence:true do
+    validates :product_name
+    validates :explain
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :state_id
+      validates :delivery_fee_id
+      validates :days_to_ship_id
+    end
+    validates :area_id, numericality: { only_integer: true }
+    validates :fee, inclusion: {in: 300..9999999}, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' }
+    validates :image
   end
-  validates :area_id, numericality: { only_integer: true }, presence: true
-  with_options presence: true, inclusion: {in: 300..9999999}, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } do
-    validates :fee
-  end
-  validates :image, presence: true
 end
