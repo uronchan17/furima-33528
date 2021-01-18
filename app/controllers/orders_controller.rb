18679c0570e4e6b76c141710f2484fabb5cb191d
uronchan17 <76198482+uronchan17@users.noreply.github.com>
2021-01-18 21:34:00 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,10 @@
 class OrdersController < ApplicationController
   def index
     @product = ProductListing.find(params[:item_id])
+    @order_form = OrderForm.new
   end
 
   def create
-    binding.pry
     @order_form = OrderForm.new(purchase_params)
     @order_form.save
   end
