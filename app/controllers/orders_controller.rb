4e4205b0da3adeb78e31d94ede5035dd896110f2
uronchan17 <76198482+uronchan17@users.noreply.github.com>
2021-01-18 16:05:30 +0900

    orderのビュー作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,8 @@
 class OrdersController < ApplicationController
   def index
+    @product = ProductListing.find(params[:item_id])
+  end
+
+  def create
   end
 end
--- app/models/product_purchase_user.rb
+++
@@ -1,5 +0,0 @@
-class ProductPurchaseUser < ApplicationRecord
-  belongs_to :user
-  belongs_to :product_listing
-  has_one    :order
-end
