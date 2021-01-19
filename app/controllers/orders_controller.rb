c27b6f393ef3244d1b01e147f679fa4f5aaab7f8
uronchan17 <76198482+uronchan17@users.noreply.github.com>
2021-01-19 18:16:07 +0900

    クレジットカードの登録

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,7 @@ class OrdersController < ApplicationController
 
   def create
     @order_form = OrderForm.new(purchase_params)
+    
     @order_form.save
     redirect_to root_path
   end
@@ -13,6 +14,6 @@ class OrdersController < ApplicationController
   private
   def purchase_params
     @product = ProductListing.find(params[:item_id])
-    params.require(:order_form).permit(:post, :area_id, :municipality, :address, :building, :phone_number, :item_id).merge(user_id: current_user.id,product_listing_id: @product.id)
+    params.require(:order_form).permit(:post, :area_id, :municipality, :address, :building, :phone_number, :item_id).merge(user_id: current_user.id, product_listing_id: @product.id, token: params[:token], price: @product.fee)
   end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,6 +1,6 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :post, :area_id, :municipality, :building, :phone_number, :address, :item_id, :user_id, :product_listing_id
+  attr_accessor :post, :area_id, :municipality, :building, :phone_number, :address, :user_id, :product_listing_id, :token, :price
   VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
   with_options presence: true do
     validates :post, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
@@ -11,6 +11,12 @@ class OrderForm
   end
 
   def save
+    Payjp.api_key = "sk_test_653263e0478df5ce701bc2e1"
+    Payjp::Charge.create(
+      amount: price,
+      card: token,
+      currency: 'jpy'
+    )
     product = ProductPurchaseUser.create!(product_listing_id: product_listing_id, user_id: user_id)
     Order.create(post: post, area_id: area_id, municipality: municipality, building: building, phone_number: phone_number, address: address, product_purchase_user_id: product.id)
   end
